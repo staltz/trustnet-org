a13de97d95690e5fe6a7eb4aad05ee4d21b51933
Andre Staltz <andre@staltz.com>
2022-04-12 14:12:33 +0300

    refactor a lot

--- index.js
+++ index.js
@@ -3,10 +3,58 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const humanTime = require('human-time');
 
-async function listForOrg(octokit, options, cb) {
+class Approvals {
+  constructor() {
+    this._map = new Map();
+  }
+
+  calcWeight(date) {
+    const yearsAgo = Math.floor((Date.now() - date) / (60e3 * 60 * 24 * 365));
+    return 1 / 2 ** yearsAgo;
+  }
+
+  update(src, dst, date) {
+    if (this._map.has(src)) {
+      const map = this._map.get(src);
+      const prev = map.get(dst) || 0;
+      map.set(dst, prev + this.calcWeight(date));
+    } else {
+      this._map.set(src, new Map([[dst, this.calcWeight(date)]]));
+    }
+  }
+
+  forEach(cb) {
+    this._map.forEach((map, src) => {
+      map.forEach((weight, dst) => {
+        cb(src, dst, weight);
+      });
+    });
+  }
+}
+
+class LastActiveRegistry {
+  constructor() {
+    this._map = new Map();
+  }
+
+  update(id, date) {
+    if (this._map.has(id)) {
+      const prev = this._map.get(id);
+      if (date > prev) this._map.set(id, date);
+    } else {
+      this._map.set(id, date);
+    }
+  }
+
+  get(id) {
+    return this._map.get(id);
+  }
+}
+
+async function forEachSourceRepo(octokit, org, cb) {
   for await (const response of octokit.paginate.iterator(
     octokit.rest.repos.listForOrg,
-    options,
+    {org, type: 'sources'},
   )) {
     const page = response.data;
     for (const repo of page) {
@@ -15,14 +63,45 @@ async function listForOrg(octokit, options, cb) {
   }
 }
 
-async function listMembers(octokit, options, cb) {
+async function forEachMember(octokit, org, cb) {
   for await (const response of octokit.paginate.iterator(
     octokit.rest.orgs.listMembers,
-    options,
+    {org},
   )) {
     const page = response.data;
     for (const member of page) {
       cb(member);
+    }
+  }
+}
+
+async function forEachClosedPR(octokit, owner, repo, cb) {
+  for await (const response of octokit.paginate.iterator(
+    octokit.rest.pulls.list,
+    {owner, repo, state: 'closed'},
+  )) {
+    const page = response.data;
+    for (const _pr of page) {
+      const {data: pr} = await octokit.rest.pulls.get({
+        owner,
+        repo,
+        pull_number: _pr.number,
+      });
+      await cb(pr);
+    }
+  }
+}
+
+async function forEachApprovedReview(octokit, owner, repo, pull_number, cb) {
+  for await (const response of octokit.paginate.iterator(
+    octokit.rest.pulls.listReviews,
+    {owner, repo, pull_number},
+  )) {
+    const page = response.data;
+    for (const review of page) {
+      if (review.state === 'APPROVED') {
+        cb(review);
+      }
     }
   }
 }
@@ -35,145 +114,76 @@ async function run() {
     const blocklist = core.getMultilineInput('blocklist').map((s) => s.trim());
     const trustThreshold = parseInt(core.getInput('trustThreshold'), 10);
     const minMemberCount = parseInt(core.getInput('minMemberCount'), 10);
-    const inactiveAfter = parseInt(core.getInput('inactiveAfter'), 10);
+    const inactiveAfter = Math.max(
+      1,
+      parseInt(core.getInput('inactiveAfter'), 10),
+    );
     const octokit = github.getOctokit(token);
 
     const tooLongAgo = new Date();
     tooLongAgo.setMonth(tooLongAgo.getMonth() - inactiveAfter);
 
     const trustnet = new TrustNet();
-    const repoNames = new Set();
+    const repos = new Set();
     const members = new Set(); // username
-    const approvals = {
-      _map: new Map(),
-      calcWeight(date) {
-        const ago = humanTime(date);
-        const yearsAgo = ago.includes('year')
-          ? parseInt(ago.split('year')[0].trim(), 10)
-          : 0;
-        return 1 / 2 ** yearsAgo;
-      },
-      update(src, dst, date) {
-        if (this._map.has(src)) {
-          const map = this._map.get(src);
-          const prev = map.get(dst) || 0;
-          map.set(dst, prev + this.calcWeight(date));
-        } else {
-          this._map.set(src, new Map([[dst, this.calcWeight(date)]]));
-        }
-      },
-      forEach(cb) {
-        this._map.forEach((map, src) => {
-          map.forEach((weight, dst) => {
-            cb(src, dst, weight);
-          });
-        });
-      },
-    };
-    const lastActive = {
-      _map: new Map(),
-      update(id, date) {
-        if (this._map.has(id)) {
-          const prev = this._map.get(id);
-          if (date > prev) this._map.set(id, date);
-        } else {
-          this._map.set(id, date);
-        }
-      },
-      get(id) {
-        return this._map.get(id);
-      },
-    };
-
-    const {data: pioneerUser} = await octokit.rest.users.getByUsername({
-      username: pioneer,
-    });
-    console.log('Pioneer:', pioneerUser);
-    console.log('\n');
+    const approvals = new Approvals();
+    const lastActiveRegistry = new LastActiveRegistry();
 
     console.log('Repos:');
-    await listForOrg(octokit, {org, type: 'sources'}, (repo) => {
+    await forEachSourceRepo(octokit, org, (repo) => {
       console.log(repo.name);
-      repoNames.add(repo.name);
+      repos.add(repo.name);
     });
     console.log('\n');
 
     console.log('Members:');
-    await listMembers(octokit, {org}, (member) => {
+    await forEachMember(octokit, org, (member) => {
       console.log(member.login);
       members.add(member.login);
     });
     console.log('\n');
 
-    for (const repo of [...repoNames.values()].slice(0, 1)) {
+    for (const repo of [...repos.values()].slice(0, 3)) {
       let prsProcessed = 0;
-      for await (const response of octokit.paginate.iterator(
-        octokit.rest.pulls.list,
-        {owner: org, repo, state: 'closed'},
-      )) {
-        const page = response.data;
-        for (const _pr of page) {
-          const {data: pr} = await octokit.rest.pulls.get({
-            owner: org,
-            repo,
-            pull_number: _pr.number,
-          });
-          prsProcessed += 1;
-          if (pr.created_at) {
-            lastActive.update(pr.user.login, new Date(pr.created_at));
-          }
-          if (!pr.merged_at) continue;
-          if (pr.merged_by && pr.merged_by.id !== pr.user.id) {
-            // console.log(
-            //   'PR #' +
-            //     pr.number +
-            //     ' by ' +
-            //     pr.user.login +
-            //     ' merged by ' +
-            //     pr.merged_by.login,
-            // );
-            const src = pr.merged_by.login;
-            const dst = pr.user.login;
-            const date = new Date(pr.merged_at);
-            approvals.update(src, dst, date);
-            lastActive.update(src, date);
-          }
-          for await (const response of octokit.paginate.iterator(
-            octokit.rest.pulls.listReviews,
-            {owner: org, repo, pull_number: pr.number},
-          )) {
-            const page = response.data;
-            for (const review of page) {
-              if (review.state === 'APPROVED') {
-                // console.log(
-                //   'PR #' +
-                //     pr.number +
-                //     ' by ' +
-                //     pr.user.login +
-                //     ' approved by ' +
-                //     review.user.login +
-                //     ' ' +
-                //     review.author_association,
-                // );
-                if (
-                  !pr.merged_by ||
-                  (review.user && review.user.id !== pr.merged_by.id)
-                ) {
-                  const src = review.user.login;
-                  const dst = pr.user.login;
-                  if (!review.submitted_at) {
-                    console.warn('review missing submitted_at:', review);
-                    continue;
-                  }
-                  const date = new Date(review.submitted_at);
-                  approvals.update(src, dst, date);
-                  lastActive.update(src, date);
-                }
-              }
-            }
-          }
+      await forEachClosedPR(octokit, org, repo, async (pr) => {
+        prsProcessed += 1;
+        if (pr.created_at) {
+          lastActiveRegistry.update(pr.user.login, new Date(pr.created_at));
         }
-      }
+        if (!pr.merged_at) return;
+        if (pr.merged_by && pr.merged_by.id !== pr.user.id) {
+          // console.log(
+          //   `PR #${pr.number} by ${pr.user.login} ` +
+          //     `merged by ${pr.merged_by.login}`,
+          // );
+          const src = pr.merged_by.login;
+          const dst = pr.user.login;
+          const date = new Date(pr.merged_at);
+          approvals.update(src, dst, date);
+          lastActiveRegistry.update(src, date);
+        }
+        await forEachApprovedReview(octokit, org, repo, pr.number, (review) => {
+          // console.log(
+          //   `PR #${pr.number} by ${pr.user.login} ` +
+          //     `approved by ${review.user.login} ` +
+          //     review.author_association,
+          // );
+          if (
+            !pr.merged_by ||
+            (review.user && review.user.id !== pr.merged_by.id)
+          ) {
+            const src = review.user.login;
+            const dst = pr.user.login;
+            if (!review.submitted_at) {
+              console.warn('review missing submitted_at:', review);
+              return;
+            }
+            const date = new Date(review.submitted_at);
+            approvals.update(src, dst, date);
+            lastActiveRegistry.update(src, date);
+          }
+        });
+      });
       console.log(`PRs in ${repo}: ${prsProcessed}`);
     }
     console.log('\n');
@@ -197,14 +207,14 @@ async function run() {
           username,
           trust,
           isMember: members.has(username),
-          lastActiveDate: lastActive.get(username),
+          lastActive: lastActiveRegistry.get(username),
         }));
 
       for (const person of persons) {
         console.log(
           person.username,
           person.trust,
-          humanTime(person.lastActiveDate),
+          humanTime(person.lastActive),
           person.isMember ? 'MEMBER' : '',
         );
       }
@@ -217,12 +227,12 @@ async function run() {
         if (
           !person.isMember &&
           person.trust >= trustThreshold &&
-          person.lastActiveDate > tooLongAgo
+          person.lastActive > tooLongAgo
         ) {
           core.notice(
             `${person.username} should be made a member ` +
               `(trustnet ${person.trust.toFixed(1)} >= ${trustThreshold} ` +
-              `and last active ${humanTime(person.lastActiveDate)})`,
+              `and last active ${humanTime(person.lastActive)})`,
             {title: 'Add member'},
           );
           wouldBeMembers.add(person.username);
@@ -234,12 +244,12 @@ async function run() {
       for (const person of persons) {
         if (
           person.isMember &&
-          person.lastActiveDate < tooLongAgo &&
+          person.lastActive < tooLongAgo &&
           wouldBeMembers.size > minMemberCount
         ) {
           core.notice(
             `${person.username} should be removed ` +
-              `(last active ${humanTime(person.lastActiveDate)})`,
+              `(last active ${humanTime(person.lastActive)})`,
             {title: 'Remove member'},
           );
           wouldBeMembers.delete(person.username);
